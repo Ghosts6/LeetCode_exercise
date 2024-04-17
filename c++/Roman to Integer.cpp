a03c8f5d79fb816551058c7e20527ed35b53e53c
Ghosts6 <kiarash82.42@gmail.com>
2024-04-17 12:56:34 +0100

    Add new file: Roman to Intger.cpp

---
+++ c++/Roman to Integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    std::unordered_map<char, int> romanToIntMap = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+public:
+    int romanToInt(std::string s) {
+        int result = 0;
+        
+        for (int i = 0; i < s.size(); ++i) {
+            if (i < s.size() - 1 && romanToIntMap[s[i]] < romanToIntMap[s[i+1]]) {
+                result -= romanToIntMap[s[i]];
+            }
+            else {
+                result += romanToIntMap[s[i]];
+            }
+        }
+        
+        return result;
+    }
+};
