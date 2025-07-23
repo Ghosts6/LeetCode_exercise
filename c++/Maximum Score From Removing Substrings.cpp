aaa15fdd2e2854d82ffeb079fd6fb945d14f8998
ghosts6 <kiarash82.42@gmail.com>
2025-07-22 21:06:03 -0400

    daily task

--- c++/Maximum Score From Removing Substrings.cpp
+++ c++/Maximum Score From Removing Substrings.cpp
@@ -4,6 +4,54 @@
 #include <cassert>
 #include <stack>
 
+// class Solution {
+// public:
+//     int maximumGain(std::string s, int x, int y) {
+//         if (x < y) {
+//             std::swap(x, y);
+//             std::swap(removeFirst, removeSecond);
+//         }
+//         return removeAndScore(s, x, y, removeFirst, removeSecond);
+//     }
+
+// private:
+//     std::string removeFirst = "ab";
+//     std::string removeSecond = "ba";
+
+//     int removeAndScore(std::string &s, int x, int y, const std::string &firstPair, const std::string &secondPair) {
+//         std::stack<char> stack;
+//         int score = 0;
+
+//         for (char c : s) {
+//             if (!stack.empty() && stack.top() == firstPair[0] && c == firstPair[1]) {
+//                 stack.pop();
+//                 score += x;
+//             } else {
+//                 stack.push(c);
+//             }
+//         }
+
+//         std::string remaining;
+//         while (!stack.empty()) {
+//             remaining.push_back(stack.top());
+//             stack.pop();
+//         }
+//         std::reverse(remaining.begin(), remaining.end());
+
+//         for (char c : remaining) {
+//             if (!stack.empty() && stack.top() == secondPair[0] && c == secondPair[1]) {
+//                 stack.pop();
+//                 score += y;
+//             } else {
+//                 stack.push(c);
+//             }
+//         }
+
+//         return score;
+//     }
+// };
+
+// optimize solution:
 class Solution {
 public:
     int maximumGain(std::string s, int x, int y) {
@@ -11,45 +59,37 @@ public:
             std::swap(x, y);
             std::swap(removeFirst, removeSecond);
         }
-        return removeAndScore(s, x, y, removeFirst, removeSecond);
-    }
 
-private:
-    std::string removeFirst = "ab";
-    std::string removeSecond = "ba";
-
-    int removeAndScore(std::string &s, int x, int y, const std::string &firstPair, const std::string &secondPair) {
-        std::stack<char> stack;
         int score = 0;
+        std::string buffer;
 
         for (char c : s) {
-            if (!stack.empty() && stack.top() == firstPair[0] && c == firstPair[1]) {
-                stack.pop();
+            if (!buffer.empty() && buffer.back() == removeFirst[0] && c == removeFirst[1]) {
+                buffer.pop_back();
                 score += x;
             } else {
-                stack.push(c);
+                buffer.push_back(c);
             }
         }
 
-        std::string remaining;
-        while (!stack.empty()) {
-            remaining.push_back(stack.top());
-            stack.pop();
-        }
-        std::reverse(remaining.begin(), remaining.end());
-
-        for (char c : remaining) {
-            if (!stack.empty() && stack.top() == secondPair[0] && c == secondPair[1]) {
-                stack.pop();
+        std::string finalBuffer;
+        for (char c : buffer) {
+            if (!finalBuffer.empty() && finalBuffer.back() == removeSecond[0] && c == removeSecond[1]) {
+                finalBuffer.pop_back();
                 score += y;
             } else {
-                stack.push(c);
+                finalBuffer.push_back(c);
             }
         }
 
         return score;
     }
+
+private:
+    std::string removeFirst = "ab";
+    std::string removeSecond = "ba";
 };
+
 
 // Tests case
 void test() {
