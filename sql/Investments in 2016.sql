072e00a6e7f47661827a927e8a4021671e8c6184
ghosts6 <kiarash82.42@gmail.com>
2026-01-09 19:01:16 -0500

    daily task(query)

--- sql/Investments in 2016.sql
+++ sql/Investments in 2016.sql
@@ -1,3 +1,4 @@
+--- base
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016 FROM Insurance
 WHERE
     tiv_2015 IN (
@@ -12,3 +13,15 @@ WHERE
         GROUP BY lat, lon
         HAVING COUNT(*) > 1
     );
+
+--- optimize
+SELECT
+    ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM (
+    SELECT
+        tiv_2016,
+        COUNT(*) OVER(PARTITION BY tiv_2015) AS tiv_count,
+        COUNT(*) OVER(PARTITION BY lat, lon) AS loc_count
+    FROM Insurance
+) AS sub
+WHERE tiv_count > 1 AND loc_count = 1;
