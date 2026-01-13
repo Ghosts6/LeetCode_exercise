7447b984372286b8137fd15068d4b6219416d1c1
ghosts6 <kiarash82.42@gmail.com>
2026-01-13 16:25:13 -0500

    daily task

---
+++ JavaCode/MinStack.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        
+        System.out.println("Current Min: " + minStack.getMin()); // Returns -3
+        minStack.pop();
+        System.out.println("Top Element: " + minStack.top());    // Returns 0
+        System.out.println("Current Min: " + minStack.getMin()); // Returns -2
+    }
+}
